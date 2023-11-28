39e03bc683134e3e2315738d3c258dabbd787036
webbariflolabs <138091633+webbariflolabs@users.noreply.github.com>
2023-11-28 14:14:52 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 # coding=utf-8
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "mqtt"
+plugin_identifier = "plugin"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_mqtt"
+plugin_package = "octoprint_plugin"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-MQTT"
+plugin_name = "DataStorage"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.8.13"
+plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
@@ -24,7 +24,7 @@ plugin_author = "Gina Häußge"
 plugin_author_email = "gina@octoprint.org"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/OctoPrint/OctoPrint-MQTT"
+plugin_url = "https://github.com/webbariflolabs/DataStorage"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
